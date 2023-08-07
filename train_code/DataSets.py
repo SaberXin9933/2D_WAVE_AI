40064734f10acf1a1053a3333b9f0576a17d2198
fengxin21 <fengxin21@meituan.com>
2023-08-07 17:55:32 +0800

    add

--- train_code/DataSets.py
+++ train_code/DataSets.py
@@ -1,15 +1,52 @@
 import torch
-import numpy
-import Params
+from Params import Params
+from Random import fRandom
+from Source import fSource
+from Domain import Domain
+from typing import *
+
+
 class DataSets:
-    def __init__(self,params:Params.Params) -> None:
+    def __init__(self,params:Params) -> None:
         self.params = params
         # 实验数据
         self.data_p = torch.zeros(params.dataset_size,1,params.width,params.height)
         self.data_v = torch.zeros(params.dataset_size,2,params.width,params.height)
-        self.
+        self.data_propagation = self.getPropagation()
+        # 计算域信息配置
+        self.domainList = self.getDomainList(params)
 
 
-    def getPML(params:Params.Params)->torch.Tensor:
-        data_pml = torch.zeros(params.dataset_size,1,params.width,params.height)
+    '''随机传播域'''
+    def getPropagation(params:Params)->torch.Tensor:
+        data_propagation = torch.zeros(1,params.width,params.height)
+        randFlag = fRandom.rand()*100000000
+        data_propagation[:] = torch.Tensor(fRandom.generate_random_z_cond(randFlag,params.width,params.height))
+        return data_propagation
+    
+    '''获取震源信息'''
+    def getSourceList(params:Params)->torch.Tensor:
+        dataSourceListNP = fSource.getRandomPointSourceList(params.dataset_size,params.dataset_size,params.minT,params.maxT,params.minBias,params.maxBias)
+        return torch.Tensor(dataSourceListNP)
+    
+    '''获取计算域配置'''
+    def getSingleDomainList(self,params:Params)->List[Domain]:
+        dataSourceList = self.getSourceList(params)
+        domainList= List[Domain]
+        for index in range(params.dataset_size):
+            source = dataSourceList[index]
+            data_p = torch.zeros(1,params.width,params.height)
+            data_v = torch.zeros(2,params.width,params.height)
+            data_propagation = self.getPropagation(params)
+            domain = Domain(index,source,0,data_p,data_v,data_propagation)
+            domainList.append(domain)
+        return domainList
+    
+    '''重制计算域名'''
+    
+
+
+
+    
+
         
--- train_code/Random.py
+++ train_code/Random.py
@@ -1,33 +1,36 @@
-import torch
-import torch.nn.functional as F
 import matplotlib.pyplot as plt
 import numpy as np
 
 
 class Random:
     def generate_random_z_cond(self, t, w, h):
-        x_mesh, y_mesh = torch.meshgrid(
-            [torch.arange(0, w), torch.arange(0, h)])
+        x_mesh, y_mesh = np.meshgrid(np.arange(0, w), np.arange(0, h))
         x_mesh, y_mesh = 1.0 * x_mesh, 1.0 * y_mesh
-        data = torch.sin(x_mesh * 0.02 + np.cos(y_mesh * 0.01 * (np.cos(t * 0.0021) + 2)) * np.cos(t * 0.01) * 3 + np.cos(
+        data = np.sin(x_mesh * 0.02 + np.cos(y_mesh * 0.01 * (np.cos(t * 0.0021) + 2)) * np.cos(t * 0.01) * 3 + np.cos(
             x_mesh * 0.011 * (np.sin(t * 0.00221) + 2)) * np.cos(t * 0.00321) * 3 + 0.01 * y_mesh * np.cos(t * 0.0215))
-        data = (1 + data/torch.max(data))/2
+        data = (5 + data/np.max(data))/5
+        data /= np.mean(data)
 
         return data
+    
+    def rand():
+        return np.random.rand()
 
     def main(self):
-        t = 1000
+        t = 10000
         w = 200
         h = 200
         mesh = self.generate_random_z_cond(t, w, h)
+        print(np.mean(mesh))
         plt.matshow(mesh)
         plt.colorbar()
         plt.show()
         y = mesh[100].squeeze()
-        x = torch.arange(y.numel())
-        plt.plot(x.detach().numpy(), y.detach().numpy())
+        x = np.arange(y.size)
+        plt.plot(x,y)
         plt.show()
 
+fRandom = Random()
 
 if __name__ == "__main__":
     r = Random()
