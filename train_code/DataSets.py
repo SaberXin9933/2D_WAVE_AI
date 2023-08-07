2382f3afe7cab258b88801d93a0ab22d66cff3ab
peak_star_rr <644109232@qq.com>
2023-08-08 00:01:29 +0800

    add

--- train_code/DataSets.py
+++ train_code/DataSets.py
@@ -7,46 +7,43 @@ from typing import *
 
 
 class DataSets:
-    def __init__(self,params:Params) -> None:
+    def __init__(self, params: Params) -> None:
         self.params = params
         # 实验数据
-        self.data_p = torch.zeros(params.dataset_size,1,params.width,params.height)
-        self.data_v = torch.zeros(params.dataset_size,2,params.width,params.height)
+        self.data_p = torch.zeros(
+            params.dataset_size, 1, params.domainWidth, params.domainHeight)
+        self.data_v = torch.zeros(
+            params.dataset_size, 2, params.domainWidth, params.domainHeight)
         self.data_propagation = self.getPropagation()
         # 计算域信息配置
         self.domainList = self.getDomainList(params)
 
-
     '''随机传播域'''
-    def getPropagation(params:Params)->torch.Tensor:
-        data_propagation = torch.zeros(1,params.width,params.height)
+    def getPropagation(params: Params) -> torch.Tensor:
+        data_propagation = torch.zeros(
+            1, params.domainWidth, params.domainHeight)
         randFlag = fRandom.rand()*100000000
-        data_propagation[:] = torch.Tensor(fRandom.generate_random_z_cond(randFlag,params.width,params.height))
+        data_propagation[:] = torch.Tensor(
+            fRandom.generate_random_z_cond(randFlag, params.domainWidth, params.domainHeight))
         return data_propagation
-    
-    '''获取震源信息'''
-    def getSourceList(params:Params)->torch.Tensor:
-        dataSourceListNP = fSource.getRandomPointSourceList(params.dataset_size,params.dataset_size,params.minT,params.maxT,params.minBias,params.maxBias)
-        return torch.Tensor(dataSourceListNP)
-    
+
+    '''获取单个计算域配置'''
+
+    def getSingleDomain(self, params: Params, index: int) -> Domain:
+        domain = Domain(index)
+        domain.data_p = torch.zeros(1, params.domainWidth, params.domainHeight)
+        domain.data_v = torch.zeros(2, params.domainWidth, params.domainHeight)
+        domain.data_propagation = self.getPropagation(params)
+        domain.source = fSource.getRandomSource(
+            params.pointNumber, params.minT, params.maxT, params.minBiasRate, params.maxBiasRate)
+
     '''获取计算域配置'''
-    def getSingleDomainList(self,params:Params)->List[Domain]:
+
+    def getSingleDomainList(self, params: Params) -> List[Domain]:
         dataSourceList = self.getSourceList(params)
-        domainList= List[Domain]
-        for index in range(params.dataset_size):
-            source = dataSourceList[index]
-            data_p = torch.zeros(1,params.width,params.height)
-            data_v = torch.zeros(2,params.width,params.height)
-            data_propagation = self.getPropagation(params)
-            domain = Domain(index,source,0,data_p,data_v,data_propagation)
-            domainList.append(domain)
+        domainList = List[Domain]
+        # for index in range(params.dataset_size):
+
         return domainList
-    
+
     '''重制计算域名'''
-    
-
-
-
-    
-
-        
--- train_code/Params.py
+++ train_code/Params.py
@@ -2,7 +2,6 @@ import json
 import sys
 import os
 import warnings
-from config import config
 
 
 class Params(object):
@@ -10,22 +9,31 @@ class Params(object):
         '''
         实验配置
         '''
-        self.precision = 2 
+        self.precision = 2
         self.netName = ""
-        self.train_env_types = ["super_simple", "oscillator","simple"]
+        self.train_env_types = ["super_simple", "oscillator", "simple"]
         self.batch_size = 50
-        self.dataset_size:int = 1000
+        self.dataset_size: int = 1000
         self.lr = 0.0001
         self.lr_grad = 0.001
-        self.width = 200
-        self.height = 200
+        self.domainWidth = 200
+        self.domainHeight = 200
         self.pml_width = 30
         self.rho = 1
         self.c = 1
         self.delta_t = 1
         self.delta_x = 2
-        self.min_T = 20
-        self.max_T = 100
+
+        '''
+        源项
+        '''
+        self.pointNumber = 200
+        self.minT = 20
+        self.maxT = 200
+        self.minBiasRate = 0.3
+        self.maxBiasRate = 0.7
+        self.WHRate = 0.05
+        self.boundaryRate = 0.3
+
 
 p = Params()
-        
