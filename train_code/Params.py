f6bd42c48a2031ee844f1fb8fdcf7eaa2d7148c5
saber <644109232@qq.com>
2023-08-10 22:05:47 +0800

    add

--- train_code/Params.py
+++ train_code/Params.py
@@ -11,8 +11,8 @@ class Params(object):
         self.optimizerName = "Adam"
         self.netHiddenSize = 32
         self.train_env_types = ["super_simple", "oscillator", "simple"]
-        self.batch_size = 10
-        self.dataset_size: int = 1000
+        self.batch_size = 100
+        self.dataset_size: int = 10000
         self.lr = 0.0001
         self.lr_grad = 0.001
         self.domainWidth = 200
@@ -56,14 +56,14 @@ class Params(object):
         """
         上下文配置
         """
-        self.loadIndex = 5
+        self.loadIndex = -1  # "-1"
         self.name = "default"
-        self.type = "train"
-        self.index = "20230810_215738" # "20230810_215738"
+        self.type = "train"  # "train" "test"
+        self.index = None  # None "20230810_215738"
 
         """
         训练配置
         """
-        self.n_epochs = 10
-        self.n_batches_per_epoch = 10
+        self.n_epochs = 1000
+        self.n_batches_per_epoch = 10000
         self.reset_freq = 0.0003
